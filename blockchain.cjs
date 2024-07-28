02489f6fb3a1679fd090f57999aa3de924c06d4a
Caique <asapcaique7@gmail.com>
2024-07-28 01:34:51 -0300

    métodos de criação e validação de bloco

--- blockchain.cjs
+++ blockchain.cjs
@@ -38,4 +38,28 @@ class CryptoBlockchain {
     startGenesisBlock() {
         return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
     }
+
+    obtainLatestBlock() {
+        return this.blockchain[this.blockchain.length - 1];
+    }
+
+    addNewBlock(newBlock) {
+        newBlock.precedingHash = this.obtainLatestBlock().hash;
+
+        newBlock.proofOfWork(this.difficulty);
+        this.blockchain.push(newBlock);
+    }
+
+    checkChainValidity() {
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i];
+            const precedingHash = this.blockchain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.computeHash()) {
+                return false;
+            }
+            if (currentBlock.precedingHash !== precedingHash.hash) return false;
+        }
+        return true;
+    }
 }
